2f301c36ddea0d3860cabf407281dc34cf9d66d9
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-08-01 14:31:48 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,28 @@
 import React, {Component, useState} from "react";
 import "./../styles/App.css";
+  // State to manage the visibility of the paragraph
+  const [isVisible, setIsVisible] = useState(false);
 
+  // Handler function for button click
+  const handleClick = () => {
+    setIsVisible(true);
+  };
 function App() {
   return (
-    <div id="main">
-      // Do not alter the main div
+    <div>
+      {/* Button with ID 'click' */}
+      <button id="click" onClick={handleClick}>
+        Show Paragraph
+      </button>
+
+      {/* Conditional rendering of the paragraph */}
+      {isVisible && (
+        <p id="para">
+          Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy.
+        </p>
+      )}
     </div>
   );
 }
-
 
 export default App;
